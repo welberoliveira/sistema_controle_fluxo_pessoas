7de39994c3d5a7d87b60292d7fe7c589b4e3cdb2
welberoliveira <welberoliveira3@gmail.com>
2022-08-30 18:50:13 -0300

    Create home.aspx.vb

---
+++ home.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class home
+    Inherits System.Web.UI.Page
+
+    Private Sub home_Load(sender As Object, e As EventArgs) Handles Me.Load
+        lbNomeUsuario.Text = Session("usuario")
+    End Sub
+End Class
