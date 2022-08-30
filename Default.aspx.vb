e82a28c1a31f0e8da7cdfef8cab7e40d12005292
welberoliveira <welberoliveira3@gmail.com>
2022-08-30 18:49:45 -0300

    Create Default.aspx.vb

---
+++ Default.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("login.aspx")
+    End Sub
+End Class
