7d35eaa80efd16236bc9bf490a08129b4126e849
welberoliveira <welberoliveira3@gmail.com>
2022-08-30 18:51:41 -0300

    Create sair.aspx.vb

---
+++ sair.aspx.vb
@@ -0,0 +1,9 @@
+﻿
+Partial Class usuarios
+    Inherits System.Web.UI.Page
+
+    Private Sub usuarios_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Session("login") = "0"
+        Response.Redirect("login.aspx")
+    End Sub
+End Class
